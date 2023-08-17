5c87e0185d89f5a5b3e5d39a0703bc1eaaf24b51
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2023-08-17 22:39:25 +0700

    add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    ROLES = (
+        ('customer', 'Customer'),
+        ('event_organizer', 'Event Organizer'),
+        ('administrator', 'Administrator'),
+    )
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    role = models.CharField(max_length=15, choices=ROLES, default='customer')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Event(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.TextField()
+    category = models.CharField(max_length=255)
+    ticket_quantity = models.IntegerField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='pending')
+    message = models.TextField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organized_events')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class UserTicket(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class EventOrganizerProposal(models.Model):
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    )
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='pending')
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class SalesData(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
