601e41da4e427bbb8f4433e96fdd1ff9d0a0090a
Andyana Lilmuttaqina Mafaza <andyanamafaza@gmail.com>
2023-08-21 19:52:06 +0700

    feat(T10):implement administrator overview of user

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,17 +1,25 @@
 from rest_framework import permissions
 
 class IsAdminOrEventOrganizers(permissions.DjangoModelPermissions):
-    message = 'role anda tidak cukup'
-    def has_permission(self,request,view):
-        print(request)
+    message = "Insufficient role permissions."
+
+    def has_permission(self, request, view):
         if not request.user.is_authenticated:
             return False
-        return request.user.role == 'event_organizer' or request.user.role == 'administrator'
-
+        return request.user.role in ['event_organizer', 'administrator']
 
 class IsAdminOrOwner(permissions.BasePermission):
-    message = 'role anda tidak cukup'
+    message = "Insufficient role permissions."
+
     def has_object_permission(self, request, view, obj):
-        if request.method == permissions.SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
         return request.user.role == 'administrator' or request.user == obj.organizer
+
+class IsAdministrator(permissions.BasePermission):
+    message = "Insufficient role permissions. Only administrators are allowed."
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+        return request.user.role == 'administrator'
